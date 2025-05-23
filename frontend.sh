f926bb7e16eafcfedb708486b2ed05c723392c0e
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-05-22 23:57:53 -0500

    practice shell 3

--- frontend.sh
+++ frontend.sh
@@ -44,3 +44,62 @@ echo "Script started executing at: $TIMESTAMP" &>> $LOG_FILE_NAME
 # Call CHECK_ROOT function
 CHECK_ROOT
 echo ===========================================
+
+#########################################
+# Install Nginx
+#########################################
+dnf install nginx -y  &>> $LOG_FILE_NAME
+VALIDATE $? "Install Nginx"
+echo ===========================================
+
+#########################################
+# Enable nginx
+#########################################
+systemctl enable nginx &>> $LOG_FILE_NAME
+VALIDATE $? "Enable Nginx"
+echo ===========================================
+
+#########################################
+# Start nginx
+#########################################
+systemctl start nginx &>> $LOG_FILE_NAME
+VALIDATE $? "Start Nginx"
+echo ===========================================
+
+##################################################################################
+# Remove the default content that web server is serving.
+##################################################################################
+rm -rf /usr/share/nginx/html/*  &>> $LOG_FILE_NAME
+VALIDATE $? "Removing old content from HTML"
+echo ===========================================
+
+##################################################################################
+# Download the frontend content
+##################################################################################
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip  &>> $LOG_FILE_NAME
+VALIDATE $? "Download frontend content code"
+echo ===========================================
+
+#########################################
+# Extract the frontend content.
+#########################################
+cd /usr/share/nginx/html  &>> $LOG_FILE_NAME
+echo ===========================================
+
+unzip /tmp/frontend.zip  &>> $LOG_FILE_NAME
+VALIDATE $? "Unzipping frontend code"
+echo ===========================================
+
+#########################################################
+# Create Nginx Reverse Proxy Configuration. Copy code
+#########################################################
+cp /home/ec2-user/practice-expense-shell-3/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copy expense config"
+echo ===========================================
+
+#########################################
+# Restart nginx
+#########################################
+systemctl restart nginx  &>> $LOG_FILE_NAME
+VALIDATE $? "Restart Nginx"
+echo ===========================================
